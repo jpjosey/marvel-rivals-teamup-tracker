4dd11425ab9898094cce917d107faa535f66e74d
jpjosey <92425017+jpjosey@users.noreply.github.com>
2026-07-08 17:18:39 +0100

    Update app.py

--- app.py
+++ app.py
@@ -27,14 +27,14 @@ with tab_main:
       selected_vanguards = st.multiselect("Allowed", options=vanguard_options, default=vanguard_options)
   
   with col_duelist:
-      st.subheader("Duelist")
+      st.subheader("✊ Duelist")
       min_duelist = st.number_input("Min", min_value=0, max_value=6, value=1, key="min_duelist")
       max_duelist = st.number_input("Max", min_value=0, max_value=6, value=3, key="max_duelist")
       duelist_options = heroes.loc[heroes["Role"] == "Duelist", names].tolist()
       selected_duelists = st.multiselect("Allowed", options=duelist_options, default=duelist_options)
   
   with col_strategist:
-      st.subheader("Strategist")
+      st.subheader("🫴 Strategist")
       min_strategist = st.number_input("Min", min_value=0, max_value=6, value=2, key="min_strategist")
       max_strategist = st.number_input("Max", min_value=0, max_value=6, value=3, key="max_strategist")
       strategist_options = heroes.loc[heroes["Role"] == "Strategist", names].tolist()

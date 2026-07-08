4758efbc0d99af185725cf83212a78cd48a6c29c
jpjosey <92425017+jpjosey@users.noreply.github.com>
2026-07-08 16:54:48 +0100

    Update app.py

--- app.py
+++ app.py
@@ -13,19 +13,19 @@ with tab_main:
   col_vanguard, col_duelist, col_strategist = st.columns(3)
 
   with col_vanguard:
-      st.subheader("Vanguards")
-      min_vanguard = st.number_input("Min", min_value=0, max_value=6, value=1)
-      max_vanguard = st.number_input("Max", min_value=0, max_value=6, value=3)
+      st.subheader("Vanguard")
+      min_vanguard = st.number_input("Min", min_value=0, max_value=6, value=1, key="min_vanguard")
+      max_vanguard = st.number_input("Max", min_value=0, max_value=6, value=3, key="max_vanguard")
   
   with col_duelist:
-      st.subheader("Duelists")
-      min_duelist = st.number_input("Min", min_value=0, max_value=6, value=1)
-      max_duelist = st.number_input("Max", min_value=0, max_value=6, value=3)
+      st.subheader("Duelist")
+      min_duelist = st.number_input("Min", min_value=0, max_value=6, value=1, key="min_duelist")
+      max_duelist = st.number_input("Max", min_value=0, max_value=6, value=3, key="max_duelist")
   
   with col_strategist:
-      st.subheader("Strategists")
-      min_strategist = st.number_input("Min", min_value=0, max_value=6, value=2)
-      max_strategist = st.number_input("Max", min_value=0, max_value=6, value=3)
+      st.subheader("Strategist")
+      min_strategist = st.number_input("Min", min_value=0, max_value=6, value=2, key="min_strategist")
+      max_strategist = st.number_input("Max", min_value=0, max_value=6, value=3, key="max_strategist")
 
 with tab_data:
   st.dataframe(teamups)

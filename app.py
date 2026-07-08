f194300c7b95c3ca014214159d323a10a6f244e7
jpjosey <92425017+jpjosey@users.noreply.github.com>
2026-07-08 17:14:25 +0100

    Update app.py

--- app.py
+++ app.py
@@ -30,11 +30,15 @@ with tab_main:
       st.subheader("Duelist")
       min_duelist = st.number_input("Min", min_value=0, max_value=6, value=1, key="min_duelist")
       max_duelist = st.number_input("Max", min_value=0, max_value=6, value=3, key="max_duelist")
+      duelist_options = heroes.loc[heroes["Role"] == "Duelist", names].tolist()
+      selected_duelists = st.multiselect("Allowed", options=duelist_options, default=duelist_options)
   
   with col_strategist:
       st.subheader("Strategist")
       min_strategist = st.number_input("Min", min_value=0, max_value=6, value=2, key="min_strategist")
       max_strategist = st.number_input("Max", min_value=0, max_value=6, value=3, key="max_strategist")
+      strategist_options = heroes.loc[heroes["Role"] == "Strategist", names].tolist()
+      selected_strategist = st.multiselect("Allowed", options=strategist_options, default=strategist_options)
 
 
 

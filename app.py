c5ebe4d6402e70e8ebf1a5518af8f403c483c6ff
jpjosey <92425017+jpjosey@users.noreply.github.com>
2026-07-08 17:07:03 +0100

    Update app.py

--- app.py
+++ app.py
@@ -10,12 +10,20 @@ st.title("Marvel Rivals Team-Up Tracker")
 tab_main, tab_data = st.tabs(["Main", "Data"])
 
 with tab_main:
-  col_vanguard, col_duelist, col_strategist, col_misc = st.columns(4)
+  col_settings, col_vanguard, col_duelist, col_strategist = st.columns(4)
+
+  with col_settings:
+      st.subheader("Options")
+      short_names = st.toggle("Use short hero names", value=True)
+
+  names = "Short_Name" if short_names else "Hero"
 
   with col_vanguard:
       st.subheader("Vanguard")
       min_vanguard = st.number_input("Min", min_value=0, max_value=6, value=1, key="min_vanguard")
       max_vanguard = st.number_input("Max", min_value=0, max_value=6, value=3, key="max_vanguard")
+      vanguard_options = heros.loc[heroes["Role"] == "Vanguard", names].tolist()
+      selected_vanguards = st.multiselect("Allowed", options=vanguard_options, default=vanguard_options)
   
   with col_duelist:
       st.subheader("Duelist")
@@ -27,9 +35,7 @@ with tab_main:
       min_strategist = st.number_input("Min", min_value=0, max_value=6, value=2, key="min_strategist")
       max_strategist = st.number_input("Max", min_value=0, max_value=6, value=3, key="max_strategist")
 
-  with col_misc:
-      st.subheader("Misc.")
-      short_names = st.toggle("Use short hero names", value=True)
+
 
 with tab_data:
   st.dataframe(teamups)

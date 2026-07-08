2091d8df22d085786662e804ed8d71ebbd975ca8
jpjosey <92425017+jpjosey@users.noreply.github.com>
2026-07-08 16:32:55 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,11 @@
 import streamlit as st
 
 st.title("Marvel Rivals Team-Up Tracker")
+
+tab_man, tab_data = st.tabs(["Main", "Data"])
+
+with tab_main:
+  st.write("Main tab - watch this space")
+
+with tab_data:
+  st.write("Data go here soon")

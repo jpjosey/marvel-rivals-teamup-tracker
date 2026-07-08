b05df0944213a142f24103d77b6fa688bc6159e9
jpjosey <92425017+jpjosey@users.noreply.github.com>
2026-07-08 18:27:01 +0100

    Update app.py

--- app.py
+++ app.py
@@ -31,30 +31,30 @@ with tab_main:
 
   with col_vanguard:
       st.subheader("Vanguard")
-      min_vanguard = st.number_input("Min", min_value=0, max_value=6, value=1, key="min_vanguard")
-      max_vanguard = st.number_input("Max", min_value=0, max_value=6, value=3, key="max_vanguard")
+      min_vanguard = st.number_input("Min", min_value=0, max_value=6, value=2, key="min_vanguard")
+      max_vanguard = st.number_input("Max", min_value=0, max_value=6, value=2, key="max_vanguard")
       vanguard_options = heroes.loc[heroes["Role"] == "Vanguard", names].tolist()
       selected_vanguards = st.multiselect("Allowed", options=vanguard_options, default=vanguard_options)
   
   with col_duelist:
       st.subheader("Duelist")
-      min_duelist = st.number_input("Min", min_value=0, max_value=6, value=1, key="min_duelist")
-      max_duelist = st.number_input("Max", min_value=0, max_value=6, value=3, key="max_duelist")
+      min_duelist = st.number_input("Min", min_value=0, max_value=6, value=2, key="min_duelist")
+      max_duelist = st.number_input("Max", min_value=0, max_value=6, value=2, key="max_duelist")
       duelist_options = heroes.loc[heroes["Role"] == "Duelist", names].tolist()
       selected_duelists = st.multiselect("Allowed", options=duelist_options, default=duelist_options)
   
   with col_strategist:
       st.subheader("Strategist")
       min_strategist = st.number_input("Min", min_value=0, max_value=6, value=2, key="min_strategist")
-      max_strategist = st.number_input("Max", min_value=0, max_value=6, value=3, key="max_strategist")
+      max_strategist = st.number_input("Max", min_value=0, max_value=6, value=2, key="max_strategist")
       strategist_options = heroes.loc[heroes["Role"] == "Strategist", names].tolist()
       selected_strategist = st.multiselect("Allowed", options=strategist_options, default=strategist_options)
 
   with col_teamups:
       st.subheader("Team-Ups")
-      min_teamups = st.number_input("Min. Enhanced", min_value=0, max_value=6, value=3, key="min_teamups")
-      max_teamups = st.number_input("Max. Enhanced", min_value=0, max_value=6, value=6, key="max_teamups")
-      selected_teamups = st.multiselect("Allowed", options=teamup_options, default=teamup_options)
+      min_teamups = st.number_input("Min. Enhanced", min_value=0, max_value=6, value=6, key="min_teamups")
+      #max_teamups = st.number_input("Max. Enhanced", min_value=0, max_value=6, value=6, key="max_teamups")
+      #selected_teamups = st.multiselect("Allowed", options=teamup_options, default=teamup_options)
 
   with col_go:
       st.subheader("GO!")
